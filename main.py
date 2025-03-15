e46829a6df9044c0fcfb33222cae4ad5b53df0df
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-15 18:33:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 # Initializing Google Gemini AI Model 
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 
-response = gemini_model.invoke("Give me one math percentage problem for 6th gread indian student in Multipe Choice Option, also provide correct answer")
+#response = gemini_model.invoke("Give me one math percentage problem for 6th gread indian student in Multipe Choice Option, also provide correct answer")
 #print(response.content)
 
 ###########################################################################
 Math_template ="""
-Can you generate Math problem for grade 6th student on {Math_Topic} with multiple choise answer and also provide answer with explanation
+Can you generate {count} Math problem for grade 6th student on math topic {Math_Topic} with multiple choise answer and also provide answer with explanation
 """
-
-Math_prompt = PromptTemplate(template = Math_template, input_variables =['Math_Topic'])
+  Math_prompt = PromptTemplate(template = Math_template, input_variables =['count','Math_topic'])
 
 #Create LLM Chain using theprompt template and Model
 Math_chain = Math_prompt | gemini_model
@@ -33,8 +32,9 @@ st.subheader(" Generate Math Exercise for practice 🤖 ")
 
 Math_topic = st.selectbox("Choose a topic for the tweet:", ["Percentage", "LCM", "HCF"])
 st.write("You selected:", Math_topic)
+count = 1
 
 if st.button("Generate"):
-    #Math_Q = Math_chain.invoke({"Math_topic" : Math_topic})
+    Math_Q = Math_chain.invoke({"count" : number, "Math_topic" : Math_topic})
     st.write(Math_Q.content)
 

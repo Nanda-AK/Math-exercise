6a56505f678a199b4a107f04094076548f09ddc8
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-15 17:50:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ import os
 
 os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
+# Initializing Google Gemini AI Model 
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+
+response = gemini_model.invoke("Give me one math percentage problem for 6th gread indian student in Multipe Choice Option, also provide correct answer")
+#print(response.content)
+
 ### Streamlit ###
-import streamlit as st
 
 st.header(" Math Exercise ")
-
-st.subheader(" Generate Math Exercise for practice 🤖")
-
-# topic = st.text_input("Topic for tweet :")
-
-# number = st.number_input("Enter a Number of tweets 1 to 10 :", min_value = 1, max_value = 10, value = 1, step = 1)
+st.subheader(" Generate Math Exercise for practice 🤖 ")
 
 
-#if st.button("Generate"):
- #   tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
-  #  st.write(tweets.content)
-
+if st.button("Generate"):
+    #MathProblem = gemini_model.invoke({"number" : number, "topic" : topic})
+    st.write(response.content)

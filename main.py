a048d39be588ef1ae36ebb4363455637553dd44d
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-15 17:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ st.subheader(" Generate Math Exercise for practice 🤖")
 
 # number = st.number_input("Enter a Number of tweets 1 to 10 :", min_value = 1, max_value = 10, value = 1, step = 1)
 
-"""
-if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
-    st.write(tweets.content)
 
-"""
+#if st.button("Generate"):
+ #   tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
+  #  st.write(tweets.content)
+
